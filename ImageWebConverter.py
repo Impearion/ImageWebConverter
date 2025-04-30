a952ba3c7f3a543b615a5f8a3ddd12c2feb51ac7
s2310307043 <s2310307043@fhooe.at>
2025-04-30 09:47:42 +0200

    Added: + converter to print help when prompted + converter to take in various configuration arguments + files are now passed as arguments instead of looking in static directory

--- ImageWebConverter.py
+++ ImageWebConverter.py
@@ -3,44 +3,46 @@ import pillow_avif
 import os
 import sys
 import argparse
+image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.gif')
 
 def convert(image_file, path, outf, qual):
     # find files that qualify as images
-    if image_file.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif')):
+    if image_file.endswith(image_extensions):
         image_path = os.path.join(path, image_file) # get absolute path
         img = Image.open(image_path) # open file
+
         # resize image
         img = img.resize(target_resolution, Image.LANCZOS)
 
         # write image
-        output_path = os.path.join(path, os.path.splitext(image_file)[0] + "."+outf)
-        print(output_path)
-        img.save(output_path, outf, quality=qual)
+        output_path = os.path.join(path, os.path.splitext(image_file)[0] + "."+outf)    # put together filepath
+        img.save(output_path, outf, quality=qual)                                       # save image
         print(f"Resized and converted to Webp: {image_file}")
 
 # parser for argument handling
 parser = argparse.ArgumentParser(description="Python script to resize and compress images")
-parser.add_argument('--res', type=str, required=False, help='Target resolution', default="1024x800")
-parser.add_argument('--qual', type=int, required=False, help='Target quality', default=100)
-parser.add_argument('--outf', type=str, required=False, help='Target file format', default="webp")
-parser.add_argument('source', type=str, help='Source files to change. If directory is passed, all files are converted. If no source is passed, workdir is used.', default=".")
+parser.add_argument('--res', type=str, required=False, help='Target resolution, format number"x"number', default="1024x800")
+parser.add_argument('--qual', type=int, required=False, help='Target quality, format number', default=100)
+parser.add_argument('--outf', type=str, required=False, help='Target file format, format ("webp", "avif")', default="webp")
+parser.add_argument('source', type=str, nargs='?', help='Source files to change. Can be filepath or directory. If directory is passed, all files are converted. If no source is passed, workdir is used.', default=".")
 args = parser.parse_args()
 
 # grab arguments and save them
 target_resolution = (int(args.res.split("x")[0]), int(args.res.split("x")[1]))
 quality = args.qual
 path = args.source
-print(path)
 
 # convert single image
 if os.path.isfile(path):
-    image_file = path
-    convert(image_file,path, args.outf, args.qual)
+    path_processed = path.split("\\")                   # split image path
+    image_file = path_processed[-1]                     # grab filename from path
+    path = '\\'.join(path_processed[:-1])               # join remaining path back together
+    convert(image_file, path, args.outf, args.qual)     # pass image to convert function
 # convert multiple images
 elif os.path.isdir(path):
-    image_files = os.listdir(path)
-    for image_file in image_files:
-        convert(image_file, path, args.outf, args.qual)
+    image_files = os.listdir(path)                      # get all files from directory
+    for image_file in image_files:                      # for each file
+        convert(image_file, path, args.outf, args.qual) # call convert
 # invalid filepath
 else:
     print("No such file or directory")
--- README.md
+++ README.md
@@ -10,16 +10,21 @@ To run this code you require Python, Pip and the library "Pillow".
 4. pip install pillow_avif_plugin
 
 ## Usage
-1. Put the images that you want to convert into the "input_images" folder.
-2. Edit the ImageWebConverter.py (change the target_resolution and quality) and save.
-3. Open CMD
-4. Navigate to the folder where the script is stored. (CD FOLDERNAME)
-5. python ImageWebConverter.py
-6. The converted images are located in the "output_images" folder.
+Run "py ImageWebConverter.py"
 
+Help output:
+usage: ImageWebConverter.py [-h] [--res RES] [--qual QUAL] [--outf OUTF] [source]
 
-## Avif vs. Webp
-To change between avif and webp, just change webp and avif in the Code.
+Python script to resize and compress images
+
+positional arguments:
+  source       Source files to change. Can be filepath or directory. If directory is passed, all files are converted. If no source is passed, workdir is used.
+
+options:
+  -h, --help   show this help message and exit
+  --res RES    Target resolution, format number"x"number
+  --qual QUAL  Target quality, format number
+  --outf OUTF  Target file format, format ("webp", "avif")
 
 ## Image Resolution
 https://tiny-img.com/blog/best-image-size-for-website/
